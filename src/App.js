bd7523323bc4c71b7a9e45a81ac339c79fe1a11b
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2022-03-03 23:13:17 +0100

    Implemented the search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Suzana</small>
+        </footer>
+      </div>
     </div>
   );
 }
