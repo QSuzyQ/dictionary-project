4eb01b34f9c50ec3119a8d2b773d715e8f4c7cb4
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2022-03-10 14:05:36 +0100

    Integrated search query photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
 
     const pexelsApiKey = `563492ad6f9170000100000141a6ea591a984fc18821c572796cf82e`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -39,7 +52,6 @@ export default function Dictionary(props) {
       <div className="Dictionary">
         <section>
           <h1>What would you want to look up?</h1>
-
           <div className="form">
             <form onSubmit={handleSubmit}>
               <input
@@ -48,12 +60,11 @@ export default function Dictionary(props) {
                 defaultValue={props.defaultKeyword}
               />
             </form>
-            <div className="hint">
-              suggested words: love, peace, sea, life...
-            </div>
+            <div className="hint">hint: love, peace, sea, life...</div>
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
